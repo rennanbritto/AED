4c0cde142f935fa6cf7ffab994450c2739bc2913
Rennan Britto <123211462+rennanbritto@users.noreply.github.com>
2024-10-20 18:32:50 -0300

    Create 1020.c

---
+++ 1020.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main() {
+    int N, ano, mes, dia;
+      scanf("%d", &N);
+   
+      ano = N/365;
+      mes = (N%365)/30;
+      dia = ((N%365)%30);
+   
+      printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", ano, mes, dia);
+   
+      return 0;
+}
